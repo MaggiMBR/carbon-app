469f64adf96bf0a65a2e9e70d3bd0a766c934c25
MaggiMBR <maggi.bastidas@gmail.com>
2022-08-25 13:06:09 +0200

    updated project details

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import './ProjectDetails.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./ProjectDetails.css";
+import { Link } from "react-router-dom";
 
-function ProjectDetails({ id, name, internalStatus, sectors, logo, website, projectDesc, transactions }) {
+function ProjectDetails({
+  id,
+  name,
+  internalStatus,
+  sectors,
+  logo,
+  website,
+  projectDesc,
+  transactions,
+}) {
   return (
     <div>
       <p>Details:</p>
-      <div class='project-details-container'>
+      <div class="project-details-container">
         <div>{id}</div>
         <div>{name}</div>
       </div>
-      <div class='project-details-container'>
+      <div class="project-details-container">
         <div>{projectDesc}</div>
-          <img src="/images/" + {logo} ></img>
       </div>
 
       <p>Transactions:</p>
-      <div class='project-details-container'>
+      <div class="project-details-container">
         {transactions.map((transaction) => (
           <div>
-            <Link to={'/transaction/' + transaction}>{transaction}</Link>
+            <Link to={"/transaction/" + transaction}>{transaction}</Link>
           </div>
         ))}
       </div>
